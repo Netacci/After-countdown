eb6e064bf78a11d970de20b1d11d0f87f0ebac50
Administrator <Administrator@kohasaeng.local>
2021-03-31 16:35:00 +0100

    changed design, not completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,30 +5,51 @@ $(window).on('load', function () {
 });
 
 // typed
-var typed = new Typed('#typed', {
-	stringsElement: '#typed-strings',
-	typeSpeed: 70,
-	loop: true,
-	loopCount: Infinity,
-});
+// var typed = new Typed('#typed', {
+// 	stringsElement: '#typed-strings',
+// 	typeSpeed: 70,
+// 	loop: true,
+// 	loopCount: Infinity,
+// });
 
 $('.slider').slick({
+	dots: false,
+	infinite: true,
+	autoplay: true,
+	loop: true,
+	speed: 500,
+	autoplaySpeed: 2000,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	dots: false,
 	responsive: [
 		{
-			breakpoint: 700,
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+
+				arrows: false,
+			},
+		},
+		{
+			breakpoint: 600,
 			settings: {
 				slidesToShow: 1,
 				slidesToScroll: 1,
 				dots: true,
-				arrows: false,
-				infinite: true,
-				autoplay: true,
-				loop: true,
-				speed: 300,
-				autoplaySpeed: 2000,
 			},
 		},
-
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				dots: true,
+			},
+		},
 		// You can unslick at a given breakpoint now by adding:
 		// settings: "unslick"
 		// instead of a settings object
