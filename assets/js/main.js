f59a8917ff87bfda9580cd43f04086f9af8303b2
Administrator <Administrator@kohasaeng.local>
2021-04-01 11:28:18 +0100

    completed page, fixed minor issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,35 +23,35 @@ $('.slider').slick({
 	slidesToScroll: 1,
 	arrows: false,
 	dots: false,
-	responsive: [
-		{
-			breakpoint: 1024,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				infinite: true,
+	// responsive: [
+	// 	{
+	// 		breakpoint: 1024,
+	// 		settings: {
+	// 			slidesToShow: 1,
+	// 			slidesToScroll: 1,
+	// 			infinite: true,
 
-				arrows: false,
-			},
-		},
-		{
-			breakpoint: 600,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				dots: true,
-			},
-		},
-		{
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				dots: true,
-			},
-		},
-		// You can unslick at a given breakpoint now by adding:
-		// settings: "unslick"
-		// instead of a settings object
-	],
+	// 			arrows: false,
+	// 		},
+	// 	},
+	// 	{
+	// 		breakpoint: 600,
+	// 		settings: {
+	// 			slidesToShow: 1,
+	// 			slidesToScroll: 1,
+	// 			dots: true,
+	// 		},
+	// 	},
+	// 	{
+	// 		breakpoint: 480,
+	// 		settings: {
+	// 			slidesToShow: 1,
+	// 			slidesToScroll: 1,
+	// 			dots: true,
+	// 		},
+	// 	},
+	// 	// You can unslick at a given breakpoint now by adding:
+	// 	// settings: "unslick"
+	// 	// instead of a settings object
+	// ],
 });
