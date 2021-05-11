8b336c070cf94ce996834a79db2e074291351a55
Chineta Adinnu <52791481+Netacci@users.noreply.github.com>
2021-05-11 11:38:53 +0100

    fixed issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 //preloader
 $(window).on('load', function () {
-	$('.status').fadeOut();
-	$('#preloader').delay(500).fadeOut();
+  $('.status').fadeOut();
+  $('#preloader').delay(500).fadeOut();
 });
 
 // typed
@@ -13,45 +13,45 @@ $(window).on('load', function () {
 // });
 
 $('.slider').slick({
-	dots: false,
-	infinite: true,
-	autoplay: true,
-	loop: true,
-	speed: 500,
-	autoplaySpeed: 2000,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	arrows: false,
-	dots: false,
-	// responsive: [
-	// 	{
-	// 		breakpoint: 1024,
-	// 		settings: {
-	// 			slidesToShow: 1,
-	// 			slidesToScroll: 1,
-	// 			infinite: true,
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  loop: true,
+  speed: 500,
+  autoplaySpeed: 2000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  // responsive: [
+  // 	{
+  // 		breakpoint: 1024,
+  // 		settings: {
+  // 			slidesToShow: 1,
+  // 			slidesToScroll: 1,
+  // 			infinite: true,
 
-	// 			arrows: false,
-	// 		},
-	// 	},
-	// 	{
-	// 		breakpoint: 600,
-	// 		settings: {
-	// 			slidesToShow: 1,
-	// 			slidesToScroll: 1,
-	// 			dots: true,
-	// 		},
-	// 	},
-	// 	{
-	// 		breakpoint: 480,
-	// 		settings: {
-	// 			slidesToShow: 1,
-	// 			slidesToScroll: 1,
-	// 			dots: true,
-	// 		},
-	// 	},
-	// 	// You can unslick at a given breakpoint now by adding:
-	// 	// settings: "unslick"
-	// 	// instead of a settings object
-	// ],
+  // 			arrows: false,
+  // 		},
+  // 	},
+  // 	{
+  // 		breakpoint: 600,
+  // 		settings: {
+  // 			slidesToShow: 1,
+  // 			slidesToScroll: 1,
+  // 			dots: true,
+  // 		},
+  // 	},
+  // 	{
+  // 		breakpoint: 480,
+  // 		settings: {
+  // 			slidesToShow: 1,
+  // 			slidesToScroll: 1,
+  // 			dots: true,
+  // 		},
+  // 	},
+  // 	// You can unslick at a given breakpoint now by adding:
+  // 	// settings: "unslick"
+  // 	// instead of a settings object
+  // ],
 });
